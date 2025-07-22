82064821930ad790b67b4e3217ff7199edfe1c17
kyumin1227 <kyumin12271227@gmail.com>
2025-07-22 21:22:46 +0900

    Lesson: LogBack

---
+++ LogBack/src/test/java/dev/be/logback/LogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
